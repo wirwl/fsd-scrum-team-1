564002477bb5ec7f44c9da65018fee6e037aeca6
SergeyVN <blakdgak@yandex.ru>
2020-10-06 14:20:46 +0300

    Added struct mode on home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Button from '@components/Button/Button';
 import '@styles/index.scss';
+import React from 'react';
 
 const Home: React.FC = () => (
-  <>
+  <React.StrictMode>
     <h1 className='m'>Hello, World!</h1>
     <Button text='click me' theme='success' />
-  </>
+  </React.StrictMode>
 );
 
 export default Home;
