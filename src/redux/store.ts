bb2c2018bad0f7bd3e283be6166545229ec65a72
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-10-07 13:40:06 +0200

    feat: apply redux-saga middleware
    #2

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, devToolsEnhancer({}));
+const sagaMiddleware = createSagaMiddleware();
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
