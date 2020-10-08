615c3a39bf5a7f3b8947381f90f0dc02d1a784d9
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-08 21:18:19 +0300

    added icons for expanded items and pix perf css;

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import { block } from 'bem-cn';
+import SVGInline from 'react-svg-inline';
+
+const expandMoreSVG = (require('./img/expand-more-down.svg')).default as string;
 
 import "./MainNav.scss";
 
 const items = [
-  { label: 'О нас', href: '/about' },
-  { label: 'Услуги', href: '/services' },
-  { label: 'Вакансии', href: '/vacancy' },
-  { label: 'Новости', href: '/news' },
-  { label: 'Соглашения', href: '/arrangements' }
+  { label: 'О нас', href: '/about', expanded: false },
+  { label: 'Услуги', href: '/services', expanded: true },
+  { label: 'Вакансии', href: '/vacancy', expanded: false },
+  { label: 'Новости', href: '/news', expanded: false },
+  { label: 'Соглашения', href: '/arrangements', expanded: true }
 ];
 
 const mainNav = block('main-nav');
@@ -16,13 +19,22 @@ const mainNavItemSelected = mainNav('item', { selected: true });
 
 const selected = '/about';
 
+console.log(mainNav('item-icon').toString());
+
 const MainNav = () => {
   return (
     <nav className={mainNav()}>
       { items.map((item) => 
-        <li key={item.href} className={selected === item.href ? mainNavItemSelected : mainNav('item')}>
+        <li key={item.href} className={
+          selected === item.href ? mainNavItemSelected : mainNav('item')
+        }>
           <Link href={item.href}>
-            <a className={mainNav('item-link')}>{item.label}</a>
+            <>
+              <a className={mainNav('item-link')}>{item.label}</a>
+              { 
+                item.expanded && <SVGInline className={mainNav('item-icon').toString()} svg={expandMoreSVG} />
+              }
+            </>
           </Link>
         </li>
       )}
