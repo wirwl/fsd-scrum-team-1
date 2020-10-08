5ab6da21dc5d485179c9a73975f0dc3008659c02
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-08 09:54:25 +0300

    fix typo: _app.tsx ".css -> .scss" root.scss "mainFont -> main-font"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { FC } from 'react';
 
 import 'normalize.css/normalize.css';
-import '@styles/root.css';
+import '@styles/root.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
