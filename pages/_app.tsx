09a854e459b47391418ee9409bdab12598ef9c55
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-08 09:47:31 +0300

    fix: make root styles global;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { FC } from 'react';
 
 import 'normalize.css/normalize.css';
+import '@styles/root.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
