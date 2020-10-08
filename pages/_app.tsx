592b66fdff5da9afa9ad8a241eb1bf6ebc21af4d
Leonid Baryshnikov <lndbaryshnikov@gmail.com>
2020-10-08 14:29:54 +0300

    fix typo"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import type { AppProps } from 'next/app';
 import { FC } from 'react';
 
 import 'normalize.css/normalize.css';
-// import 'material-design-icons/iconfont/material-icons.css';
+import 'material-design-icons/iconfont/material-icons.css';
 
-import '@styles/fonts.scss';
+import '@styles/root.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
