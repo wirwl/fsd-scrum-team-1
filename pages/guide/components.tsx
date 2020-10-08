ecfc8c6135006e1529209c8c83d703f731a16adb
SergeyVN <blakdgak@yandex.ru>
2020-10-08 13:33:12 +0300

    Calendar added to components page.

--- pages/guide/components.tsx
+++ pages/guide/components.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 
+import '@styles/index.scss';
 import MainLayout from 'src/layouts/MainLayout';
+import Calendar from '@components/Calendar/Calendar';
 
 const Components: FC = () => (
   <MainLayout title="Components">
     <h1>Components Page</h1>
+    <Calendar />
   </MainLayout>
 );
 
