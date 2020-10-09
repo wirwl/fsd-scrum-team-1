cdff678a9151b92d6bfb734f5ccd0f0354e3c5e8
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-10-10 00:06:05 +0800

    #14 added props

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,25 @@
-import './logo.scss';
 import { block } from 'bem-cn';
+
+import './logo.scss';
 
 const b = block('logo');
 
-const Logo: React.FC = () => {
+interface ILogo {
+  href?: string;
+  alt?: string;
+  title?: string;
+}
+
+const Logo: React.FC<ILogo> = (props) => {
+  const { href = '/', alt = 'Toxin', title = 'Toxin' } = props;
+
   return (
-    <a className={b()} href="/">
+    <a className={b()} href = {href}>
       <img
-        className={b('image')}
-        src="/images/logo.svg"
-        alt="Toxin"
-        title="Toxin"
+        className = {b('image')}
+        src = "/images/logo.svg"
+        alt = {alt}
+        title = {title}
       />
     </a>
   );
