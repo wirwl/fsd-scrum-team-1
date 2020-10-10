1cfc76a4200966f0e77a3a2830ceb8e51cb11d95
Andrey Larionov <andrey.larionov.dev@gmail.com>
2020-10-10 20:45:20 +0200

    feat(input-dropdown): add reducer and format counts
    #27

--- src/components/InputDropdown/InputDropdown.tsx
+++ src/components/InputDropdown/InputDropdown.tsx
@@ -1,10 +1,17 @@
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { block } from 'bem-cn';
 import './input-dropdown.scss';
 
+interface IPlurals {
+  few: string,
+  one: string,
+  two: string,
+}
+
 interface IDropListItem {
-  text: string,
+  label: string,
   count: number,
+  plurals: IPlurals,
 }
 
 interface IInputDropdownProps {
@@ -12,20 +19,42 @@ interface IInputDropdownProps {
   placeholder?: string,
   dropList?: IDropListItem[],
   isExpanded?: boolean,
+  reducer?: (items: IDropListItem[]) => string;
 }
+
+const formatCount = (count: number, plurals: IPlurals): string => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  const titles = [plurals.one, plurals.two, plurals.few];
+
+  return titles[
+    (count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]
+  ];
+};
+
+const reduceCountsAndPlurals = (items: IDropListItem[]): string => {
+  return items.map((item) => (item.count > 0
+    ? `${item.count} ${formatCount(item.count, item.plurals)}`
+    : '')).filter(Boolean).join(', ');
+};
 
 const InputDropdown: FC<IInputDropdownProps> = ({
   name,
   placeholder = 'Выберите удобства',
   dropList = [
-    { text: 'Спальни', count: 0 },
-    { text: 'Кровати', count: 0 },
-    { text: 'Ванные комнаты', count: 0 },
+    { label: 'Спальни', count: 2, plurals: { few: 'спален', one: 'спальня', two: 'спальни' } },
+    { label: 'Кровати', count: 2, plurals: { few: 'кроватей', one: 'кровать', two: 'кровати' } },
+    { label: 'Ванные комнаты', count: 0, plurals: { few: 'ванных комнат', one: 'ванная комната', two: 'ванные комнаты' } },
   ],
   isExpanded = false,
+  reducer = reduceCountsAndPlurals,
 }) => {
   const [dropListState, setDropListState] = useState<IDropListItem[]>(dropList);
   const [isExpandedState, setIsExpandedState] = useState<boolean>(isExpanded);
+  const [valueState, setValueState] = useState<string>();
+
+  useEffect(() => {
+    setValueState(reducer(dropListState));
+  });
 
   const changeDropItemCount = (index: number, type: 'dec'|'inc'):void => {
     const items = [...dropListState];
@@ -42,8 +71,8 @@ const InputDropdown: FC<IInputDropdownProps> = ({
   };
 
   const dropElements = dropListState.map((item, index) => (
-    <li key={item.text} className={bem('list-item')}>
-      <div className={bem('item-name')}>{item.text}</div>
+    <li key={item.label} className={bem('list-item')}>
+      <div className={bem('item-name')}>{item.label}</div>
       <div className={bem('counter-buttons')}>
         <button
           type="button"
@@ -73,6 +102,8 @@ const InputDropdown: FC<IInputDropdownProps> = ({
         name={name}
         placeholder={placeholder}
         className={bem('text-input')}
+        readOnly
+        value={valueState || ''}
       />
       <button
         className={`${bem('toggle-button')} material-icons`}
