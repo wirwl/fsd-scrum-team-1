82281433badc04fd6e06887f8755d62769540d5e
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-11 11:54:24 +0300

    add banner-and-signature component;

---
+++ src/components/banner-and-signature/BannerAndSignature.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import { block } from 'bem-cn';
+
+import './banner-and-signature.scss';
+
+const b = block('banner-and-signature');
+
+type IBannerAndSignatureProps = {
+  signature: string;
+};
+
+/**
+ * Exapmle
+ * <BannerAndSignature signature="Lorem ipsum dolor sit amet consectetur adipisicing elit.">
+ *   <form>...</form>
+ * </BannerAndSigrature>
+ */
+const BannerAndSignature: FC<IBannerAndSignatureProps> = (props) => {
+  const {
+    children,
+    signature,
+  } = props;
+
+  return (
+    <section className={b()}>
+      <div className={b('center')}>
+        {children}
+        <p className={b('signature')}>{ signature }</p>
+      </div>
+    </section>
+  );
+};
+
+export default BannerAndSignature;
