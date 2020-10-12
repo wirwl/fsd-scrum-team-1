e94bcb2ff7dd2270ec378435490d493e0784792e
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-10-12 20:57:51 +0800

    #19 made corrections by Linter

--- src/components/FooterNav/FooterNav.tsx
+++ src/components/FooterNav/FooterNav.tsx
@@ -6,27 +6,22 @@ import './FooterNav.scss';
 
 type IFooterNav = {
   items: IMenu[]
-}
+};
 
 const b = block('footer-nav');
 
-const FooterNav: React.FC<IFooterNav> = ({ items }) => {
-  return (
-    <nav className = {b()}>
-      {
-        items.map((item) => {
-          return (
-            <NavMenu 
-              title = {item.title} 
-              links = {item.links} 
-              key = {item.title}
-            />
-          )
-        })
-      }
-    </nav>
-  )
-}
+const FooterNav: React.FC<IFooterNav> = ({ items }) => (
+  <nav className={b()}>
+    {
+      items.map((item) => (
+        <NavMenu
+          title={item.title}
+          links={item.links}
+          key={item.title}
+        />
+      ))
+    }
+  </nav>
+);
 
 export default FooterNav;
-
--- src/components/FooterNav/components/NavMenu/NavMenu.tsx
+++ src/components/FooterNav/components/NavMenu/NavMenu.tsx
@@ -6,33 +6,33 @@ import './NavMenu.scss';
 type IMenu = {
   title: string
   links: IMenuLinks[]
-}
+};
 
 type IMenuLinks = {
   label: string,
-  href: string  
-}
+  href: string
+};
 
 const b = block('footer-menu');
 
-const NavMenu: React.FC<IMenu> = ({ title, links }) => {
-  return (
-    <section className = {b()}>
-      <h3 className = {b('title')}> {title} </h3>
-      <div className = {b('content')}>
-        { 
-          links.map(link => {
-            return (
-              <Link href = {link.href} key = {link.label} >
-                <a className = {b('link')}> {link.label} </a>
-              </Link>
-            )
-          })
-        }
-      </div>
-    </section>
-  )  
-}
+const NavMenu: React.FC<IMenu> = ({ title, links }) => (
+  <section className={b()}>
+    <h3 className={b('title')}>
+      {title}
+    </h3>
+    <div className={b('content')}>
+      {
+        links.map((link) => (
+          <Link href={link.href} key={link.label}>
+            <a href={link.href} className={b('link')}>
+              {link.label}
+            </a>
+          </Link>
+        ))
+      }
+    </div>
+  </section>
+);
 
 export type { IMenu, IMenuLinks };
 
