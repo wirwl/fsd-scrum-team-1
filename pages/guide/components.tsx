a83c6b69e606b6f6344d6354c68271498a730a69
SergeyVN <blakdgak@yandex.ru>
2020-10-13 12:13:25 +0300

    Fixed connection of components on the page.

--- pages/guide/components.tsx
+++ pages/guide/components.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react';
 
 import MainLayout from 'src/layouts/MainLayout';
-import Input from '@components/Input/Input';
+import Input from '@components/input/Input';
 
 const Components: FC = () => (
   <MainLayout title="Components">
     <h1>Components Page</h1>
     <Input head="text field" placeholder="Email" />
-    <Input head="masked text field" placeholder="ДД.ММ.ГГГГ" mask="99.99.9999" />
-    <Input head="subscription text field" placeholder="Email" validate="email" withArrow />
+    <Input head="masked text field" placeholder="ДД.ММ.ГГГГ" mask="99.99.9999" icon="expand" />
+    <Input head="masked text field" placeholder="ДД.ММ.ГГГГ" mask="99.99.9999" icon="expand" selected readonly />
+    <Input head="subscription text field" placeholder="Email" validate="email" icon="arrow" />
   </MainLayout>
 );
 
