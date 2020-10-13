2231e388eddc00ffe13c1b8985d854a66791ad38
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-13 12:45:23 +0300

    added header auth;

---
+++ src/components/header-auth/HeaderAuth.tsx
@@ -0,0 +1,60 @@
+import type { FC } from 'react';
+import Link from 'next/link';
+import { block } from 'bem-cn';
+
+import Button from '@components/button/Button';
+
+import './header-auth.scss';
+
+type IUser = {
+  name: string
+  lastName: string
+};
+
+type IHeaderAuthProps = {
+  user: IUser | null
+};
+
+const b = block('header-auth');
+
+const HeaderAuth: FC<IHeaderAuthProps> = ({ user }) => {
+  const content = (user !== null)
+    ? (
+      <span className={b('user-name')}>
+        {user.name}
+        &nbsp;
+        {user.lastName}
+      </span>
+    )
+    : (
+      <div className={b('actions')}>
+        <div className={b('action')}>
+          <Link href="/auth/sign-in">
+            <Button
+              href="/auth/sign-in"
+              theme="white"
+              caption="Вход"
+            />
+          </Link>
+        </div>
+
+        <div className={b('action')}>
+          <Link href="/auth/register">
+            <Button
+              href="/auth/register"
+              theme="default"
+              caption="Зарегистрироваться"
+            />
+          </Link>
+        </div>
+      </div>
+    );
+
+  return (
+    <div className={b()}>
+      {content}
+    </div>
+  );
+};
+
+export default HeaderAuth;
