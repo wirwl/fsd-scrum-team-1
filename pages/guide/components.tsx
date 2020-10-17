5a3250a69f4b1403e2055f766674e8daabbc673d
Sergey Novozhilov <blakdgak1@yandex.ru>
2020-10-17 02:08:09 +0000

    Added calendar functionality.
    Added handler for clear and apply buttons.

--- pages/guide/components.tsx
+++ pages/guide/components.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 
 import MainLayout from 'src/layouts/MainLayout';
+import Calendar from '@components/Calendar/Calendar';
 
 const Components: FC = () => (
   <MainLayout title="Components">
     <h1>Components Page</h1>
+    <Calendar />
   </MainLayout>
 );
 
