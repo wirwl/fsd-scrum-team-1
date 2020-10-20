b7d642c49f6b1c384d3a0c7c57b0ee53caf2f383
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-20 15:31:40 +0300

    refactoring - move all variables to .json config;

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,58 +5,16 @@ import Logo from '@/components/Logo/Logo';
 import Input from '@/components/Input/Input';
 import SocialButtons from '@/components/social-buttons/SocialButtons';
 
-import NavigationList, { ILink } from './components/NavigationList/NavigationList';
+import {
+  navBlocks,
+  description,
+  subscription,
+  copyright,
+  socialLinks,
+} from 'config/footer.json';
+import NavigationList from './components/NavigationList/NavigationList';
 
 import './Footer.scss';
-
-type INavBlock = {
-  title: string;
-  links: ILink[];
-};
-
-const description = 'Бронирование номеров в лучшем отеле 2019 года по версии ассоциации «Отельные взгляды»';
-
-const navBlocks: INavBlock[] = [
-  {
-    title: 'Навигация',
-    links: [
-      { name: 'О нас', href: '/404' },
-      { name: 'Новости', href: '/404' },
-      { name: 'Служба поддержки', href: '/404' },
-      { name: 'Услуги', href: '/404' },
-    ],
-  },
-  {
-    title: 'О нас',
-    links: [
-      { name: 'О сервисе', href: '/404' },
-      { name: 'Наша команда', href: '/404' },
-      { name: 'Вакансии', href: '/404' },
-      { name: 'Инвесторы', href: '/404' },
-    ],
-  },
-  {
-    title: 'Служба поддержки',
-    links: [
-      { name: 'Соглашения', href: '/404' },
-      { name: 'Сообщества', href: '/404' },
-      { name: 'Связь с нами', href: '/404' },
-    ],
-  },
-];
-
-const subscription = {
-  title: 'Подписка',
-  description: 'Получайте специальные предложения и новости сервиса',
-  url: '/subscription-api',
-};
-
-const copyright = 'Copyright © 2018 Toxin отель. Все права защищены.';
-const socialLinks = [
-  { text: 'twitter', link: 'twitter.com' },
-  { text: 'facebook-square', link: 'facebook.com' },
-  { text: 'instagram', link: 'instagram.com' },
-];
 
 const b = block('footer');
 
