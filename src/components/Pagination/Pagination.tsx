7979cd87036d91b048bee30bb8caf64ac19b3d6d
guipure <igor778868@gmail.com>
2020-10-22 22:32:08 +0300

    added styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,27 +1,56 @@
+import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
 
-interface IPagination {
+import './Pagination.scss';
+
+interface IPaginationProps {
   itemCount: number,
   limit: number,
 }
 
-const Pagination: React.FC<IPagination> = ({ itemCount, limit }) => {
+type PageData = { selected: number };
+
+const Pagination: React.FC<IPaginationProps> = ({ itemCount, limit }) => {
   const pageCount = Math.ceil(itemCount / limit);
+  const totalCount = itemCount > 100 ? '100+' : itemCount.toString();
+
+  const [currentRange, setCurrentRange] = useState([1, limit]);
+
+  const handlePageClick = ({ selected }: PageData): void => {
+    const fromItemCount = selected * limit + 1;
+    const toItemCount = fromItemCount + limit - 1;
+
+    setCurrentRange([fromItemCount, toItemCount]);
+  };
 
   return (
-    <ReactPaginate
-      previousLabel="previous"
-      nextLabel="next"
-      breakLabel="..."
-      breakClassName="break-me"
-      pageCount={pageCount}
-      marginPagesDisplayed={2}
-      pageRangeDisplayed={5}
-      // onPageChange={this.handlePageClick}
-      containerClassName="pagination"
-      // subContainerClassName={'pages pagination'}
-      activeClassName="active"
-    />
+    <div className="pagination">
+      <ReactPaginate
+        previousLabel="arrow_back"
+        nextLabel="arrow_forward"
+        breakLabel="..."
+        pageCount={pageCount}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={2}
+        onPageChange={handlePageClick}
+        containerClassName="pagination__wrapper"
+        pageClassName="pagination__cell"
+        breakClassName="pagination__cell"
+        activeClassName="pagination__cell pagination__cell_active"
+        nextClassName="pagination__cell pagination__cell_with-arrow"
+        previousClassName="pagination__cell pagination__cell_with-arrow"
+        pageLinkClassName="pagination__link"
+        breakLinkClassName="pagination__link"
+        activeLinkClassName="pagination__link pagination__link_active"
+        previousLinkClassName="pagination__link pagination__link_with-arrow"
+        nextLinkClassName="pagination__link pagination__link_with-arrow"
+        disabledClassName="pagination__cell_disabled"
+      />
+      <span className="pagination__text">
+        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+        {currentRange[0]}–{currentRange[1]} из {totalCount} вариантов аренды
+      </span>
+    </div>
   );
 };
 
