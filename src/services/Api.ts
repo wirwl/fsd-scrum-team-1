e8b689d89e1ab2b9139aaa840df3849d0b889fb1
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-23 16:38:39 +0300

    add Rooms dto; set up firebase for server;

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,21 +1,43 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import * as admin from 'firebase-admin';
 
 import firebaseConfig from '../../config/firebase.json';
 
-class Api {
-  auth: firebase.auth.Auth;
+const isClientAndNotInit = (
+  apps: firebase.app.App[],
+): boolean => process.browser && apps.length === 0;
 
-  firestore: firebase.firestore.Firestore;
+class Api {
+  auth: firebase.auth.Auth | admin.auth.Auth;
+
+  firestore: firebase.firestore.Firestore | admin.firestore.Firestore;
 
   constructor() {
-    if (firebase.apps.length === 0) {
+    if (isClientAndNotInit(firebase.apps)) {
       firebase.initializeApp(firebaseConfig);
     }
 
-    this.auth = firebase.auth();
-    this.firestore = firebase.firestore();
+    if (process.browser) {
+      this.firestore = firebase.firestore();
+      this.auth = firebase.auth();
+      // this.auth.signInWithEmailAndPassword('mglkn@yandex.ru', '123456');
+      return;
+    }
+
+    // server only
+    if (admin.apps.length === 0) {
+      admin.initializeApp({
+        credential: admin.credential.cert(
+          require('../../config/fsd-toxin-firebase-adminsdk-dafjv-bfab20e08e.json'), // eslint-disable-line
+        ),
+        databaseURL: 'https://fsd-toxin.firebaseio.com',
+      });
+    }
+
+    this.firestore = admin.firestore();
+    this.auth = admin.auth();
   }
 
   // TODO: implements api methods
