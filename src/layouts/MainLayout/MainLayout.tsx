4628e856eab21876eb78e3d830a26127911e08d2
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-10-24 22:52:19 +0800

    #63 Добавил favicons, метатеги

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -25,6 +25,13 @@ const MainLayout: FC<IMainLayoutProps> = ({
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta charSet="utf-8" />
+      <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+      <link rel="manifest" href="favicons/site.webmanifest" />
+      <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <StrictMode>
       <body className={b()}>
