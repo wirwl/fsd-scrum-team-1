1a10df0ae7871ed7a8ef7719417944f413398234
Dmitry <wirwl@hotmail.com>
2020-10-24 22:54:15 +0600

    Code refactoring in likeButton and BulltesList

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
+import LikeButton from '@/components/LikeButton/LikeButton';
 import { FC } from 'react';
 
 import BannerAndSignature from 'src/components/BannerAndSignature/BannerAndSignature';
 import MainLayout from 'src/layouts/MainLayout/MainLayout';
+import '../src/components/LikeButton/LikeButton';
 
 const PAGE_SIGNATURE = 'Лучшие номера для вашей работы, отдыха и просто вдохновения';
 const TITLE = 'Toxin';
 
 const Main: FC = () => (
   <MainLayout title={TITLE}>
+    <LikeButton />
     <BannerAndSignature signature={PAGE_SIGNATURE}>
       awesome form
     </BannerAndSignature>
--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -1,32 +1,41 @@
 /* eslint-disable */
 import './LikeButton.scss';
 import block from 'bem-cn';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 
 const b = block('like-button');
 
 interface ILikeButton {
-  isChecked: boolean;
-  count: number;
+  isChecked?: boolean;
+  count?: number;
+  onChange?: (values: ILikeButton) => void;
 }
 
-const LikeButton: React.FC<ILikeButton> = (props) => {
+const LikeButton: FC<ILikeButton> = (props) => {
   const {
     isChecked = false,
     count = 0,
+    onChange
   } = props;
 
   const [state, setState] = useState<ILikeButton>({ isChecked, count });
 
   const handleLikeButtonClick = (): void => {
     setState((prev) => {
-      const { isChecked: isCheckedPrev, count: countPrev } = prev;
+      const {
+        isChecked: isCheckedPrev = false,
+        count: countPrev = 0
+      } = prev;
 
       const newValue = isCheckedPrev ? countPrev - 1 : countPrev + 1;
-      return {
+      const result = {
         isChecked: !isCheckedPrev,
         count: newValue,
       };
+
+      onChange && onChange(result);
+
+      return result;
     });
   };
 
