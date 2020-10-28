cad8c2eba55d056b567ea68e1908b8054129584b
Vladimir Migalkin <mglkn@yandex.ru>
2020-10-28 19:21:53 +0300

    setup landing page;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import { FC } from 'react';
+import { block } from 'bem-cn';
 
-import BannerAndSignature from 'src/components/BannerAndSignature/BannerAndSignature';
 import MainLayout from 'src/layouts/MainLayout/MainLayout';
+import BannerAndSignature from 'src/components/BannerAndSignature/BannerAndSignature';
+import FormLanding from 'src/components/FormLanding/FormLanding';
+
+import './index.scss';
 
 const PAGE_SIGNATURE = 'Лучшие номера для вашей работы, отдыха и просто вдохновения';
 const TITLE = 'Toxin';
 
-const Main: FC = () => (
+const b = block('index');
+
+const Index: FC = () => (
   <MainLayout title={TITLE}>
     <BannerAndSignature signature={PAGE_SIGNATURE}>
-      awesome form
+      <section className={b()}>
+        <FormLanding onSubmit={() => {}} />
+      </section>
     </BannerAndSignature>
   </MainLayout>
 );
 
-export default Main;
+export default Index;
