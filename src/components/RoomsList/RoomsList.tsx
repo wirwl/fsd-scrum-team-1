5990986caf86f21cecfa9c24be6bf399b99be019
Daniil <daniil.petro3@gmail.com>
2020-11-03 15:42:10 +0500

    add props

--- src/components/RoomsList/RoomsList.tsx
+++ src/components/RoomsList/RoomsList.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import { block } from 'bem-cn';
+
 import { IRootState } from 'src/redux/reducer';
 import { IRoom } from 'src/services/dto/Rooms';
 import RoomCard, { IRoomCardProps } from 'src/components/RoomCard/RoomCard';
@@ -8,6 +9,10 @@ import Button from 'src/components/Button/Button';
 import './RoomsList.scss';
 
 const b = block('rooms');
+
+interface IRoomsListProps {
+  onShowMoreButtonClick: () => void;
+}
 
 const convertDataForRoomCard = (inputData: IRoom): IRoomCardProps => {
   const {
@@ -37,7 +42,9 @@ const convertDataForRoomCard = (inputData: IRoom): IRoomCardProps => {
   return roomCardData;
 };
 
-const RoomsList: FC = () => {
+const RoomsList: FC<IRoomsListProps> = (props) => {
+  const { onShowMoreButtonClick } = props;
+
   const { isFetching, items: rooms, error } = useSelector(
     (state: IRootState) => state.rooms,
     shallowEqual,
@@ -54,17 +61,37 @@ const RoomsList: FC = () => {
   });
 
   const isFailLoad = error && !isFetching;
-
   const isEmpty = rooms.length === 0;
+
+  const handleClick = (evt: MouseEvent): void => {
+    evt.preventDefault();
+    onShowMoreButtonClick();
+  };
+
+  const getCorrectElement = (): JSX.Element => {
+    if (isFetching) return <div>Loading...</div>;
+
+    if (isEmpty) {
+      return (
+        <div>
+          К сожалению, у нас нет номеров, которые соответствовали бы всем вашим критериям.
+          Постарайтесь изменить фильтры.
+        </div>
+      );
+    }
+
+    if (isFailLoad) return <div>{error}</div>;
+
+    return <ul className={b('list')}>{roomCards}</ul>;
+  };
 
   return (
     <section className={b()}>
       <h2 className={b('title')}>Номера, которые мы для вас подобрали</h2>
-      <ul className={b('list')}>{isFetching ? <div>Loading...</div> : roomCards}</ul>
-      {isFailLoad && <div>{error}</div>}
+      {getCorrectElement()}
       {!isEmpty && (
         <div className={b('show-more')}>
-          <Button theme="textual" caption="Показать еще" />
+          <Button theme="textual" caption="Показать еще" handleClick={handleClick} />
         </div>
       )}
     </section>
