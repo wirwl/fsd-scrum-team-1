93e75e735799650e110c5d8897b443bb0b8f9d2c
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-04 18:25:42 +0300

    add CheckboxGroup component;

---
+++ src/components/CheckboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,76 @@
+import { FC, useEffect, useState } from 'react';
+import { block } from 'bem-cn';
+
+import Checkbox from 'src/components/Checkbox/Checkbox';
+
+import './CheckboxGroup.scss';
+
+const b = block('checkbox-group');
+
+type ICheckboxGroupProps = {
+  title: string;
+  items: ICheckboxProps[];
+  onChange: (values: { [key:string]: boolean }) => void;
+};
+
+const CheckboxGroup: FC<ICheckboxGroupProps> = ({ title, items, onChange }) => {
+  const [state, setState] = useState<{ [key:string]: boolean }>(
+    () => items.reduce(
+      (acc, { name, checked }: ICheckboxProps) => ({
+        ...acc,
+        [name]: checked !== undefined && checked,
+      }),
+      {},
+    ),
+  );
+
+  const handleCheckboxChange = (value: boolean, name: string): void => {
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    onChange(state);
+  }, [state]);
+
+  const checkboxItems = items.map((item: ICheckboxProps) => {
+    const {
+      label,
+      name,
+      description,
+    } = item;
+
+    const componentItem = description !== undefined
+      ? (
+        <Checkbox
+          label={label}
+          name={name}
+          description={description}
+          onChange={handleCheckboxChange}
+        />
+      )
+      : (
+        <Checkbox
+          label={label}
+          name={name}
+          onChange={handleCheckboxChange}
+        />
+      );
+
+    return <li className={b('item')} key={label}>{ componentItem }</li>;
+  });
+
+  return (
+    <div className={b()}>
+      <h3 className={b('title')}>{ title }</h3>
+
+      <ul className={b('list')}>
+        { checkboxItems }
+      </ul>
+    </div>
+  );
+};
+
+export default CheckboxGroup;
