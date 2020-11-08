d82bcf5b4c8408c1c00f7a6bb176472e7d43c819
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-08 14:33:22 +0300

    refactoring: move init values to separate file;

--- src/components/FormRoomsFilter/FormRoomsFilter.tsx
+++ src/components/FormRoomsFilter/FormRoomsFilter.tsx
@@ -1,4 +1,8 @@
-import { FC, useState, useEffect } from 'react';
+import {
+  FC,
+  useState,
+  useEffect,
+} from 'react';
 import { block } from 'bem-cn';
 
 import type { IDropListItem } from 'src/components/InputDropdown/InputDropdown';
@@ -21,120 +25,17 @@ import {
   initCheckboxGroupState,
   initDropdownState,
 } from 'src/components/FormRoomsFilter/helpers';
+import {
+  dropdownGuestsInit,
+  dropdownConvinenceInit,
+  extraConvinienceInit,
+  rulesInit,
+  accessibilityInit,
+} from 'src/components/FormRoomsFilter/initValues';
 
 import './FormRoomsFilter.scss';
 
 const b = block('form-rooms-filter');
-
-const dropdownGuestsInit: IDropListItem[] = [
-  {
-    id: 'adults',
-    label: 'Взрослые',
-    count: 0,
-    plurals: { one: 'гость', two: 'гостя', few: 'гостей' },
-  },
-  {
-    id: 'children',
-    label: 'Дети',
-    count: 0,
-    plurals: { one: 'гость', two: 'гостя', few: 'гостей' },
-  },
-  {
-    id: 'babies',
-    label: 'Младенцы',
-    count: 0,
-    plurals: { one: 'младенец', two: 'младенца', few: 'младенцев' },
-    special: true,
-  },
-];
-
-const dropdownItemsConvinenceInit: IDropListItem[] = [
-  {
-    id: 'bedroom',
-    label: 'Спальни',
-    count: 0,
-    plurals: { one: 'спальня', two: 'спальни', few: 'спален' },
-  },
-  {
-    id: 'bed',
-    label: 'Кровати',
-    count: 0,
-    plurals: { one: 'кровать', two: 'кровати', few: 'кроватей' },
-  },
-  {
-    id: 'bathroom',
-    label: 'Ванные комнаты',
-    count: 0,
-    plurals: { one: 'ванная комната', two: 'ванные комнаты', few: 'ванных комнат' },
-    special: true,
-  },
-];
-
-const extraConvinienceInit = [
-  {
-    label: 'Завтрак',
-    name: 'breakfast',
-    checked: false,
-  },
-  {
-    label: 'Письменный стол',
-    name: 'desk',
-    checked: false,
-  },
-  {
-    label: 'Стул для кормления',
-    name: 'feedingChair',
-    checked: false,
-  },
-  {
-    label: 'Кроватка',
-    name: 'smallBad',
-    checked: false,
-  },
-  {
-    label: 'Телевизор',
-    name: 'tv',
-    checked: false,
-  },
-  {
-    label: 'Шампунь',
-    name: 'shampoo',
-    checked: false,
-  },
-];
-
-const rulesInit = [
-  {
-    label: 'Можно курить',
-    name: 'smokingAllowed',
-    checked: false,
-  },
-  {
-    label: 'Можно с питомцами',
-    name: 'petsAllowed',
-    checked: false,
-  },
-  {
-    label: 'Можно пригласить гостей (до 10 человек)',
-    name: 'guestsAllowed',
-    checked: false,
-  },
-];
-
-const accessibilityInit = [
-  {
-    label: 'Широкий коридор',
-    name: 'wideCorridor',
-    description: 'Ширина коридоров в номере не менее 91 см.',
-    checked: false,
-  },
-  {
-    label: 'Помощник для инвалидов',
-    name: 'assistantForDisabled',
-    description: 'На 1 этаже вас встретит специалист  и проводит до номера.',
-    checked: false,
-  },
-];
 
 type IFormRoomFilterState = {
   adults?: number;
@@ -185,7 +86,7 @@ const initState = (query: Record<string, string>): IFormRoomFilterState => {
   if (dateRange !== null) result.dateRange = dateRange;
 
   const guests = initDropdownState(query, dropdownGuestsInit);
-  const convinience = initDropdownState(query, dropdownItemsConvinenceInit);
+  const convinience = initDropdownState(query, dropdownConvinenceInit);
   result = { ...result, ...guests, ...convinience };
 
   const rules = initCheckboxGroupState('rules', query, rulesInit);
@@ -357,7 +258,7 @@ const FormRoomsFilter: FC<IFormRoomFilterProps> = ({ query, onChange }) => {
           name="convinience"
           placeholder="Удобства номера"
           defaultLabel={false}
-          initValues={dropdownItemsConvinenceInit}
+          initValues={dropdownConvinenceInit}
           onChange={handleConvinienceDropdownChange}
         />
       </section>
