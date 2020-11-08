9d0af44a0b5ad607a6353b242f920bb57a720056
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-08 16:10:11 +0300

    remove initial for datepicker

--- src/components/FormRoomsFilter/components/DatePickerFilter/DatePickerFilter.tsx
+++ src/components/FormRoomsFilter/components/DatePickerFilter/DatePickerFilter.tsx
@@ -1,9 +1,7 @@
 import {
   FC,
-  useState,
 } from 'react';
 import DatePicker from 'src/components/DatePicker/DatePicker';
-import { initDateRangeState } from 'src/components/FormRoomsFilter/helpers';
 
 type IDateRangeFilter = {
   start: number;
@@ -15,17 +13,13 @@ type IDatePickerFilterProps = {
   onChange: (dateRange: IDateRangeFilter, dateRangeString: string) => void;
 };
 
-const DatePickerFilter: FC<IDatePickerFilterProps> = ({ query, onChange }) => {
-  // Need initialize DatePicker when this feature will be implemented
-  const [_state, setState] = useState(initDateRangeState(query));
-
+const DatePickerFilter: FC<IDatePickerFilterProps> = ({ onChange }) => {
   const handleDatePickerChange = ({ start, end }: RangeDays): void => {
     const newValue = {
       start: (new Date(start as Date)).getTime(),
       end: (new Date(end as Date)).getTime(),
     };
 
-    setState(newValue);
     onChange(newValue, `${newValue.start}-${newValue.end}`);
   };
 
