1eab74b1f56d17d77ed703a6765f38d585d7dfff
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-10 17:24:12 +0300

    rewrite validation;

--- src/components/FormSignIn/FormSignIn.tsx
+++ src/components/FormSignIn/FormSignIn.tsx
@@ -13,62 +13,97 @@ type IFormSignInProps = {
 
 const b = block('form-sign-in');
 
-type IInputValuesState = {
-  email: string;
-  isEmailValid: boolean;
-  password: string;
-  isPasswordValid: boolean;
+type IValueState = {
+  value: string;
+  isTouched: boolean;
+  error: string | null;
 };
 
-const isFormValid = (
-  { isEmailValid, isPasswordValid }: IInputValuesState,
-): boolean => isEmailValid && isPasswordValid;
+type IInputValuesState = Record<string, IValueState>;
+
+const passwordValidate: CustomValidateFunction = (pass) => {
+  const password = pass as string;
+  if (password.length < 6) {
+    return 'Пароль слишком короткий (меньше 6 символов)';
+  }
+
+  if (password.length > 20) {
+    return 'Пароль слишком длинный (больше 20 символов)';
+  }
+
+  return null;
+};
+
+const isFormValid = (values: IInputValuesState): boolean => (
+  Object.keys(values).reduce(
+    (acc: boolean, key: string) => (
+      acc && values[key].error === null && values[key].isTouched
+    ),
+    true,
+  )
+);
+
+const initValues = {
+  email: {
+    value: '',
+    isTouched: false,
+    error: null,
+  },
+  password: {
+    value: '',
+    isTouched: false,
+    error: null,
+  },
+};
+
+const normalizeErrorMessage = (
+  error: string | null,
+): string | undefined => (
+  error === null ? undefined : error
+);
 
 const FormSignIn: FC<IFormSignInProps> = ({ onSubmit }) => {
-  const [values, setValues] = useState<IInputValuesState>({
-    email: '',
-    isEmailValid: false,
-    password: '',
-    isPasswordValid: false,
-  });
+  const [values, setValues] = useState<IInputValuesState>(
+    initValues,
+  );
+
+  const { password, email } = values;
+
+  const setUntouchedErrors = (): void => {
+    Object.keys(values).forEach((key) => {
+      if (!values[key].isTouched) {
+        setValues((prevValue) => ({
+          ...prevValue,
+          [key]: {
+            ...values[key],
+            error: 'Это поле обязательно для заполнения',
+          },
+        }));
+      }
+    });
+  };
 
   const handleFormSubmit = (e: FormEvent): void => {
     e.preventDefault();
 
-    isFormValid(values) && onSubmit(values.email, values.password);
+    setUntouchedErrors();
+    if (isFormValid(values)) {
+      onSubmit(email.value, password.value);
+    }
   };
 
-  const passwordValidate: CustomValidateFunction = (password) => {
-    if (password.toString().length + 1 <= 6) {
-      return 'Пароль слишком короткий (меньше 6 символов)';
-    }
-
-    if (password.toString().length - 1 >= 20) {
-      return 'Пароль слишком длинный (больше 20 символов)';
-    }
-
-    return null;
-  };
-
-  const handleEmailInputChange = (
-    email: string,
-    isInvalid: boolean,
+  const handleInputChange = (
+    value: string,
+    name: string,
+    error: string | null,
   ): void => {
     setValues((prevValues) => ({
       ...prevValues,
-      email,
-      isEmailValid: !isInvalid,
-    }));
-  };
-
-  const handlePasswordInputChange = (
-    password: string,
-    isInvalid: boolean,
-  ): void => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      password,
-      isPasswordValid: !isInvalid,
+      [name]: {
+        value,
+        isTouched: true,
+        error,
+      },
     }));
   };
 
@@ -79,19 +114,22 @@ const FormSignIn: FC<IFormSignInProps> = ({ onSubmit }) => {
       <div className={b('input')}>
         <Input
           type="email"
-          onChange={handleEmailInputChange}
+          name="email"
+          onChange={handleInputChange}
           placeholder="Email"
           validate="email"
-          emailValidationErrorMessage="Не правильный email"
+          errorMessage={normalizeErrorMessage(email.error)}
         />
       </div>
 
       <div className={b('input')}>
         <Input
           type="password"
-          onChange={handlePasswordInputChange}
+          name="password"
+          onChange={handleInputChange}
           placeholder="Пароль"
           validate={passwordValidate}
+          errorMessage={normalizeErrorMessage(password.error)}
         />
       </div>
 
