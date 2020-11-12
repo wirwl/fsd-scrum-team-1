23ac357cd280357003530864063bad8081a1cd7d
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-12 17:39:55 +0300

    remove useless code from userSaga;

--- src/redux/user/userSaga.ts
+++ src/redux/user/userSaga.ts
@@ -4,30 +4,15 @@ import { takeLatest, put } from 'redux-saga/effects';
 import Api, { getAuthError } from 'src/services/Api';
 import {
   SIGN_IN,
-  SIGN_IN_SUCCESS,
-  SIGN_IN_FAIL,
-  SIGN_OUT_SUCCESS,
+  // SIGN_OUT_SUCCESS,
 } from 'src/redux/user/userTypes';
 import {
   signIn,
   signInRequesting,
-  signInSuccess,
   signInFail,
 } from 'src/redux/user/userActions';
 
 const api = new Api();
-
-function* signInSuccessSaga(
-  { payload }: ReturnType<typeof signInSuccess>,
-) : SagaIterator {
-  yield put(signInSuccess(payload));
-}
-
-// function* signInFailSaga(
-//   { payload }: ReturnType<typeof signInFail>,
-// ) : SagaIterator {
-//   yield put(signInFail(payload));
-// }
 
 const DEFAULT_MESSAGE = 'Что-то пошло не так, проверьте соединение и перезагрузите страницу';
 function* signInSaga(
@@ -55,8 +40,6 @@ function* signInSaga(
 
 function* watchUserSaga(): SagaIterator {
   yield takeLatest(SIGN_IN, signInSaga);
-  yield takeLatest(SIGN_IN_SUCCESS, signInSuccessSaga);
-  // yield takeLatest(SIGN_IN_FAIL, signInFailSaga);
 }
 
 export default watchUserSaga;
