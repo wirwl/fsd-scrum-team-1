3b79488cc1db80975e9839ba60dc300adc5a1884
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-14 21:45:21 +0300

    add signin page;

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
+import { block } from 'bem-cn';
 
 import MainLayout from 'src/layouts/MainLayout/MainLayout';
+import FormSignIn from 'src/components/FormSignIn/FormSignIn';
+
+import 'pages/auth/SignIn.scss';
+
+const b = block('sign-in');
 
 const SignIn: FC = () => (
   <MainLayout title="Sign In">
-    <h1>Sign In Page</h1>
+    <div className={b()}>
+      <section className={b('layout')}>
+        <FormSignIn onSubmit={() => {}} />
+      </section>
+    </div>
   </MainLayout>
 );
 
