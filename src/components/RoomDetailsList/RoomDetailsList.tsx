8473bd960419540e62428c2caa449c29a838ae21
Daniil <daniil.petro3@gmail.com>
2020-11-17 13:13:00 +0500

    relocate map into separate var

--- src/components/RoomDetailsList/RoomDetailsList.tsx
+++ src/components/RoomDetailsList/RoomDetailsList.tsx
@@ -18,26 +18,30 @@ interface IRoomDetailsList {
 const RoomDetailsList: FC<IRoomDetailsList> = ({
   header,
   roomDetails,
-}) => (
-  <div className={b()}>
-    <h3 className={b('header')}>{header}</h3>
-    <ul className={b('list')}>
-      {roomDetails.map((roomDetail) => {
-        const { icon, caption, description } = roomDetail;
-        return (
-          <li className={b('item')} key={icon}>
-            <div className={b('icon').mix('material-icons')}>
-              {icon}
-            </div>
-            <div className={b('wrapper')}>
-              <div className={b('caption')}>{caption}</div>
-              <p className={b('description')}>{description}</p>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-);
+}) => {
+  const roomDetailsItems = roomDetails.map((roomDetail) => {
+    const { icon, caption, description } = roomDetail;
+    return (
+      <li className={b('item')} key={icon}>
+        <div className={b('icon').mix('material-icons')}>
+          {icon}
+        </div>
+        <div className={b('wrapper')}>
+          <div className={b('caption')}>{caption}</div>
+          <p className={b('description')}>{description}</p>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div className={b()}>
+      <h3 className={b('header')}>{header}</h3>
+      <ul className={b('list')}>
+        {roomDetailsItems}
+      </ul>
+    </div>
+  );
+};
 
 export default RoomDetailsList;
