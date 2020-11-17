927351c23068035673630929946b6e62f80271de
Daniil <daniil.petro3@gmail.com>
2020-11-17 13:43:33 +0500

    refactor details block for api

--- src/components/RoomDetailsList/RoomDetailsList.tsx
+++ src/components/RoomDetailsList/RoomDetailsList.tsx
@@ -1,31 +1,47 @@
 import './RoomDetailsList.scss';
 import block from 'bem-cn';
 import { FC } from 'react';
+import { IRoomInformation } from 'src/services/dto/Rooms';
 
 const b = block('room-details-list');
 
 interface IRoomDetail {
-  icon: string,
-  caption: string,
-  description: string
+  icon: string;
+  caption: string;
+  description: string;
 }
 
 interface IRoomDetailsList {
-  header: string,
-  roomDetails: IRoomDetail[]
+  header: string;
+  roomDetails: IRoomInformation[];
 }
 
-const RoomDetailsList: FC<IRoomDetailsList> = ({
-  header,
-  roomDetails,
-}) => {
+type RoomDetailedMap = Record<IRoomInformation, IRoomDetail>;
+
+const roomDetailedMap: RoomDetailedMap = {
+  comfort: {
+    icon: 'tag_faces',
+    caption: 'Комфорт',
+    description: 'Шумопоглощающие стены',
+  },
+  convinience: {
+    icon: 'location_city',
+    caption: 'Удобство',
+    description: 'Окно в каждой из спален',
+  },
+  cozy: {
+    icon: 'whatshot',
+    caption: 'Уют',
+    description: 'Номер оснащён камином',
+  },
+};
+
+const RoomDetailsList: FC<IRoomDetailsList> = ({ header, roomDetails }) => {
   const roomDetailsItems = roomDetails.map((roomDetail) => {
-    const { icon, caption, description } = roomDetail;
+    const { icon, caption, description } = roomDetailedMap[roomDetail];
     return (
       <li className={b('item')} key={icon}>
-        <div className={b('icon').mix('material-icons')}>
-          {icon}
-        </div>
+        <div className={b('icon').mix('material-icons')}>{icon}</div>
         <div className={b('wrapper')}>
           <div className={b('caption')}>{caption}</div>
           <p className={b('description')}>{description}</p>
@@ -37,9 +53,7 @@ const RoomDetailsList: FC<IRoomDetailsList> = ({
   return (
     <div className={b()}>
       <h3 className={b('header')}>{header}</h3>
-      <ul className={b('list')}>
-        {roomDetailsItems}
-      </ul>
+      <ul className={b('list')}>{roomDetailsItems}</ul>
     </div>
   );
 };
--- src/services/dto/Rooms.ts
+++ src/services/dto/Rooms.ts
@@ -72,4 +72,5 @@ export type {
   IRoomImpressions,
   IRoomComment,
   IRoomCommentAuthor,
+  IRoomInformation,
 };
