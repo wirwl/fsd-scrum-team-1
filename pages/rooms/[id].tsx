b6c93a0a9bc23d8e202cca2414ed6604719697b2
SergeyVN <blakdgak@yandex.ru>
2020-11-18 18:54:32 +0300

    Fixed data loading.

--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -1,8 +1,8 @@
-import { FC, useEffect } from 'react';
-import { GetServerSideProps } from 'next';
-import { useDispatch, useSelector } from 'react-redux';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
 import { block } from 'bem-cn';
 
+import wrapper, { ISagaStore } from '@/redux/store';
 import MainLayout from 'src/layouts/MainLayout/MainLayout';
 import { fetchRoomDetails } from '@/redux/roomDetails/roomDetailsActions';
 import { IRootState } from '@/redux/reducer';
@@ -16,25 +16,21 @@ import RoomCancel from '@/components/RoomCancel/RoomCancel';
 import FormRoomDetails from '@/components/FormRoomDetails/FormRoomDetails';
 
 import './roomDetails.scss';
+import { END } from 'redux-saga';
 
 type IRoomDetailsProps = { id: string | string[] | undefined };
 
 const b = block('room-details');
 
-const RoomDetails: FC<IRoomDetailsProps> = ({ id }) => {
-  const { isFetching, item: room } = useSelector((state: IRootState) => state.roomDetails);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchRoomDetails({ id: String(id) }));
-  }, []);
+const RoomDetails: FC<IRoomDetailsProps> = () => {
+  const { item: room } = useSelector((state: IRootState) => state.roomDetails);
 
   const pageContent = room === null
     ? <Spinner />
     : (
       <div className={b()}>
         <section className={b('gallery')}>
-          <RoomGallery photos={room.pics.map((src) => ({ src, alt: src }))} />
+          <RoomGallery photos={room.pics.slice(-3).map((src) => ({ src, alt: src }))} />
         </section>
         <div className={b('container-content')}>
           <section className={b('room-info')}>
@@ -87,16 +83,18 @@ const RoomDetails: FC<IRoomDetailsProps> = ({ id }) => {
     );
 
   return (
-    <MainLayout title={`Room ${isFetching && room ? room.roomNumber : 'Details'}`}>
+    <MainLayout title={`Room ${room ? room.roomNumber : 'Details'}`}>
       {pageContent}
     </MainLayout>
   );
 };
 
-const getServerSideProps: GetServerSideProps = async (context) => ({
-  props: { id: context.query.id },
+export const getServerSideProps = wrapper.getServerSideProps(async ({ store, query }) => {
+  const { id } = query;
+  store.dispatch(fetchRoomDetails({ id: String(id) }));
+  store.dispatch(END);
+  await (store as ISagaStore).sagaTask?.toPromise();
+  return {};
 });
-
-export { getServerSideProps };
 
 export default RoomDetails;
