c150e8a6fdd83be4280653d696cc426d85180506
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-23 21:02:21 +0300

    add locales

---
+++ locales/es.ts
@@ -0,0 +1,195 @@
+const es = {
+  shared: {
+    enter: 'Entrar',
+    exit: 'Salida',
+    register: 'Registrarse',
+    create: 'Crear',
+    clean: 'Limpio',
+    apply: 'Aplicar',
+    rules: 'Reglas',
+    cancel: 'Cancelar',
+    sign: 'Las mejores salas para tu trabajo, ocio e inspiración',
+    allRight: 'Copyright © 2018 Hotel Toxin. Todos los derechos reservados.',
+    perDay: 'por día',
+    luxe: 'lujo',
+    arrival: 'llegada',
+    departure: 'salida',
+    name: 'Nombre',
+    lastname: 'Apellido',
+    man: 'hombre',
+    woman: 'mujer',
+    birthday: 'birthday',
+    password: 'cumpleaños',
+    getSpecial: 'Reciba ofertas especiales',
+    personalInformation: 'Información personal',
+    bookedRooms: 'Cuartos reservados',
+
+    nav: {
+      navigation: 'navegación',
+      subscription: 'suscripción',
+      aboutUs: 'Sobre nosotros',
+      aboutServece: 'about service',
+      services: 'sobre el servicio',
+      supports: 'Servicio de soporte',
+      communities: 'Comunidades',
+      investors: 'Inversores',
+      ourTeem: 'salir lleno',
+      jobs: 'trabajos',
+      news: 'noticias',
+      agreements: 'Acuerdos',
+      booking: 'reserva',
+      discountNotification: 'Avisos sobre descuentos',
+      luggageDelivery: 'Entrega de equipaje',
+      carpsFishing: 'Pesca de carpas en la piscina',
+      communicationWithUs: 'Comunicación con nosotros',
+    },
+  },
+
+  footer: {
+    description: 'Reserva de habitaciones en el mejor hotel de 2019 según la Asociación "Vistas de hotel"',
+    subscriptionMessage: 'Reciba ofertas especiales y noticias de servicio',
+  },
+
+  forms: {
+    landing: {
+      title: 'Encontraremos habitaciones para sus necesidades',
+      pickANumber: 'elige un número',
+    },
+
+    BookingForm: {
+      day: 'dias',
+      feeServiceDiscount: 'La tarifa por el servicio: descuento',
+      feeAdditionalService: 'Tarifa por servicios adicionales',
+      subtotal: 'Total parcial',
+    },
+
+    Register: {
+      title: 'Registro de cuenta',
+      loginInformation: 'Información de inicio de sesión para el servicio',
+    },
+
+    Enter: {
+      title: 'Entrar',
+    },
+  },
+
+  components: {
+    guestInputDropdown: {
+      title: 'invitados',
+
+      guests: 'invitados',
+      guestsOne: 'invitado',
+      guestTwo: 'invitados',
+      guestsFew: 'invitados',
+
+      adults: 'adultos',
+      children: 'niños',
+
+      babies: 'bebés',
+      babiesOne: 'bebé',
+      babiesTwo: 'bebés',
+      babiesFew: 'bebés',
+    },
+
+    convenientInputDropdown: {
+      title: 'Instalaciones de la habitación',
+
+      bedrooms: 'dormitorios',
+      bedroomOne: 'habitación',
+      bedroomsTwo: 'dormitorios',
+      bedroomsFew: 'dormitorios',
+
+      beds: 'camas',
+      bedOne: 'cama',
+      bedsTwo: 'camas',
+      bedsFew: 'camas',
+
+      bathrooms: 'baños',
+      bathroomOne: 'baño',
+      bathroomsTwo: 'baños',
+      bathroomsFew: 'baños',
+    },
+
+    range: {
+      priceRange: 'rango de precios',
+      description: 'Precio por noche de estancia en la habitación',
+    },
+
+    rules: {
+      title: 'Reglas de casa',
+      petsAllowed: 'Puede estar con mascotas',
+      smokingAllowed: 'Puedes fumar',
+      guestAllowed: 'Puedes invitar a invitados (hasta 10 personas).',
+    },
+
+    accessibility: {
+      title: 'Accesibilidad',
+      wideCorridor: {
+        label: 'Pasillo ancho',
+        description: 'El ancho de los pasillos de la habitación es de al menos 91 cm.',
+      },
+      assistantForDisabled: {
+        label: 'Asistente para discapacitados',
+        description: 'Un especialista lo recibirá en el primer piso y lo guiará a su habitación.',
+      },
+    },
+
+    extraConvenience: {
+      title: 'Mayor comodidad',
+
+      breakfast: 'Desayuno',
+      desk: 'Escritorio',
+      feedingChair: 'Silla de alimentación',
+      smallBad: 'Cuna',
+      tv: 'TV',
+      shampoo: 'Champú',
+    },
+  },
+
+  rooms: {
+    title: 'Habitaciones que hemos seleccionado para ti',
+
+    comments: 'comentarios',
+  },
+
+  room: {
+    roomInfo: {
+      title: 'Información de la habitación',
+
+      comfort: {
+        title: 'Comodidad',
+        description: 'Paredes absorbentes de ruido',
+      },
+
+      convenience: {
+        title: 'Conveniencia',
+        description: 'Ventana en cada uno de los dormitorios',
+      },
+
+      cozy: {
+        title: 'Acogedor',
+        description: 'Esta habitación tiene chimenea.',
+      },
+    },
+
+    roomImpressions: {
+      title: 'Impresiones de las habitaciones',
+
+      wonderful: 'Excelente',
+      good: 'Bueno',
+      satisfactorily: 'Satisfactoriamente',
+      bad: 'Decepcionado',
+    },
+
+    roomComments: {
+      title: 'Reseñas de usuarios de la habitación',
+      daysAgo: 'hace días',
+
+      feedbackOne: 'comentario',
+      feedbackTwo: 'comentarios',
+      feedbackFew: 'comentarios',
+    },
+  },
+};
+
+export default es;
---
+++ locales/index.ts
@@ -0,0 +1,6 @@
+import ru from './en';
+import ru from './ru';
+
+const phrasesByLocale: { en: typeof en, ru: typeof ru } = { en, ru };
+
+export { en, ru, phrasesByLocale };
