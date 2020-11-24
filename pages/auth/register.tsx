a2a5ac2c3b198dd16655ee5161ba2ecfd7510cef
Daniil <daniil.petro3@gmail.com>
2020-11-24 17:22:25 +0500

    add registration page

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
+import { block } from 'bem-cn';
 
 import MainLayout from 'src/layouts/MainLayout/MainLayout';
+import RegistrationForm from 'src/components/RegistrationForm/RegistrationForm';
+
+import './Register.scss';
+
+const b = block('registration');
 
 const Register: FC = () => (
   <MainLayout title="Register">
-    <h1>Register Page</h1>
+    <section className={b()}>
+      <div className={b('form')}>
+        <RegistrationForm onSubmit={() => {}} />
+      </div>
+    </section>
   </MainLayout>
 );
 
