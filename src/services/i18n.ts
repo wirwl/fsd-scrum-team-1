046a72c9e1ca45546677da9433c381fd656717ba
Vladimir Migalkin <mglkn@yandex.ru>
2020-11-24 16:49:22 +0300

    add langs to config, fix en translates

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -6,9 +6,11 @@ const { localeSubpaths } = NextConfig().publicRuntimeConfig;
 
 const DEFAULT_LANG = 'ru';
 
+const OTHER_LANG = ['en', 'de', 'es'];
+
 const i18n = new NextI18Next({
   defaultLanguage: DEFAULT_LANG,
-  otherLanguages: ['en'],
+  otherLanguages: OTHER_LANG,
   localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
   browserLanguageDetection: true,
